e1ba58aebac52e7058469b4b835aa1125d9e298d
miz-ab <mizaby19@gmail.com>
2024-12-03 23:02:12 +0300

    add second / alt host address

--- src/main/java/com/zbs/zb/constants/Constants.java
+++ src/main/java/com/zbs/zb/constants/Constants.java
@@ -3,6 +3,7 @@ package com.zbs.zb.constants;
 public class Constants {
 
     public static final String EXTERNAL_HOST_ADDRESS = "http://127.0.0.1:3012";
+    public static final String EXTERNAL_HOST_ADDRESS_2 = "http://127.0.0.1:3012";
     public static final String FIRST_DAY_OF_THE_MONTH = "01";
     public static final String CHAR_NUMERIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     public static final String CREDIT_VALUE_CODE = "CRE";
